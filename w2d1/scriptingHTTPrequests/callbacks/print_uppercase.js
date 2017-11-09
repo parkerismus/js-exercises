2e423f03d136936f66364eb916a2d05115a3e2f4
parkerismus <31933785+parkerismus@users.noreply.github.com>
2017-11-09 18:06:47 +0000

    Step 6

---
+++ w2d1/scriptingHTTPrequests/callbacks/print_uppercase.js
@@ -0,0 +1,14 @@
+var https = require('https');
+
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase(html) {
+  console.log(html.toUpperCase());
+}
+
+getHTML(requestOptions, printUpperCase);
