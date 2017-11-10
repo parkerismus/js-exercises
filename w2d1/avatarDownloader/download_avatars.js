4ef7c2dcd59951727c5682edaed172a7c18101c5
parkerismus <31933785+parkerismus@users.noreply.github.com>
2017-11-10 01:25:55 +0000

    with getRepoContributors object and for url and headers

--- w2d1/avatarDownloader/download_avatars.js
+++ w2d1/avatarDownloader/download_avatars.js
@@ -1,8 +1,20 @@
 var request = require('request');
+var secrets = require('./secrets.js');
+
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  //
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets["GITHUB_TOKEN"]
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 
